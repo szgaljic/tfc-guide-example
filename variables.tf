d51920311b7df23b7e8c9394a7530995f6319b3d
Steven Zgaljic <szgaljic@gmail.com>
2020-04-15 12:44:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,18 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-1"
-}
-
-variable "db_table_name" {
-  type    = string
-  default = "exampleTable"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
-
 variable "tag_user_name" {
   type = string
+}
+
+variable "ec2_ami" {
+  type    = string
+  default = "ubuntu-bionic-18.04-amd64-server-20200112"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ec2_instance_type" {
+  type    = string
+  default = "t2.micro"
 }
