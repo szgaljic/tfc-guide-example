f262a594635328d0f96dac51a172420dee65438b
Steven Zgaljic <szgaljic@gmail.com>
2020-04-15 14:44:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,11 @@
 provider "aws" {
   version = "2.33.0"
-
   region = var.aws_region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["amazon", "aws-marketplace"]
-  
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/${var.ec2_ami}"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
-
   tags = {
     user_name = var.tag_user_name
   }
