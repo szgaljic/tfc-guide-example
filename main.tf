9d6a98f5ae6919af9696a4c8e033c877fd3c88e7
Steven Zgaljic <szgaljic@gmail.com>
2020-04-15 12:42:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2_instance_type
 
   tags = {
